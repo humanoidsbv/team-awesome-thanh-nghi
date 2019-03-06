0ae246a4441b7dbc2bd18f2313795307fb284cd4
TNT Humanoid <tnthumanoid@Thanhs-MacBook-Pro.local>
2019-03-06 10:23:42 +0100

    processed review changes

--- index.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.querySelector('.menu-button');
-menuButton.onclick = function (event) {
-    console.log(event);
-    document.querySelector.classList.toggle('.header--navigation-visible');
-};
