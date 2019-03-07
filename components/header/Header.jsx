ad55ae4791e9c924f43eededafc8e9f994048c4b
TNT Humanoid <tnthumanoid@Thanhs-MacBook-Pro.local>
2019-03-07 17:30:29 +0100

    Improve header and navigation components

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -5,11 +5,24 @@ import ProfileButton from '../profile-button/ProfileButton';
 
 import './header.scss';
 
-const Header = () => (
-  <header className="header">
-    <Navigation />
-    <ProfileButton />
-  </header>
-);
+class Header extends React.Component {
+  state = {
+    isNavigationOpen: false
+  };
+
+  toggleNavigation = () => {
+    this.setState(({ isNavigationOpen }) => ({ isNavigationOpen: !isNavigationOpen }));
+  }
+
+  render() {
+    const { isNavigationOpen } = this.state;
+    return (
+      <header className={`header ${isNavigationOpen ? 'header--navigation-visible' : 'header--navigation-invisible'}`}>
+        <Navigation handleClick={this.toggleNavigation} />
+        <ProfileButton />
+      </header>
+    );
+  }
+}
 
 export default Header;
