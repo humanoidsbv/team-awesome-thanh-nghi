0afc379dc55b5dc17c2762d32b2a8c2375858abd
TNT Humanoid <tnthumanoid@Thanhs-MacBook-Pro.local>
2019-03-13 22:32:05 +0100

    Proces PR review changes

--- components/new-time-entry/NewTimeEntry.jsx
+++ components/new-time-entry/NewTimeEntry.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 import './new-time-entry.scss';
 import NewEntryButton from '../new-entry-button';
--- pages/index.js
+++ pages/index.js
@@ -9,25 +9,25 @@ import TimeEntries from '../components/time-entries';
 
 const timeEntryData = [
   {
-    id: '1',
-    date: '29-07-2018',
     client: 'Port of Rotterdam',
-    startTime: '09:00',
-    endTime: '17:00'
+    date: '29-07-2018',
+    endTime: '17:00',
+    id: '1',
+    startTime: '09:00'
   },
   {
-    id: '2',
-    date: '30-07-2018',
     client: 'Hike One',
-    startTime: '10:00',
-    endTime: '18:00'
+    date: '30-07-2018',
+    endTime: '18:00',
+    id: '2',
+    startTime: '10:00'
   },
   {
-    id: '3',
-    date: '31-07-2018',
     client: 'ING Bank',
-    startTime: '09:00',
-    endTime: '18:00'
+    date: '31-07-2018',
+    endTime: '18:00',
+    id: '3',
+    startTime: '09:00'
   }
 ];
 
