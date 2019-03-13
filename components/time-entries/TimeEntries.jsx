44c8b8f4ecdf5b533764e4ad75b2782a6caf6f32
TNT Humanoid <tnthumanoid@Thanhs-MacBook-Pro.local>
2019-03-13 10:32:28 +0100

    Create new branch mocked data entry

--- components/time-entries/TimeEntries.jsx
+++ components/time-entries/TimeEntries.jsx
@@ -3,11 +3,32 @@ import React from 'react';
 import './time-entries.scss';
 import TimeEntry from '../time-entry';
 
-const TimeEntries = () => (
-  <React.Fragment>
-    <TimeEntry date="29-07-2018" client="Port of Rotterdam" startTime="09:00" endTime="17:00" />
-    <TimeEntry date="30-07-2018" client="Hike One" startTime="10:00" endTime="18:00" />
-  </React.Fragment>
-);
+const timeEntryData = [
+  {
+    date: '29-07-2018',
+    client: 'Port of Rotterdam',
+    startTime: '09:00',
+    endTime: '17:00'
+  },
+  {
+    date: '30-07-2018',
+    client: 'Hike One',
+    startTime: '10:00',
+    endTime: '18:00'
+  }
+];
+
+class TimeEntries extends React.Component {
+  state = { timeEntries: timeEntryData }
+
+  render() {
+    return (
+      <React.Fragment>
+        <TimeEntry date="29-07-2018" client="Port of Rotterdam" startTime="09:00" endTime="17:00" />
+        <TimeEntry date="30-07-2018" client="Hike One" startTime="10:00" endTime="18:00" />
+      </React.Fragment>
+    );
+  }
+}
 
 export default TimeEntries;
