37f67721984fc8ccee340651d32c513b95e3f55e
TNT Humanoid <tnthumanoid@Thanhs-MacBook-Pro.local>
2019-03-21 14:26:05 +0100

    Restore prompt confirm function before deleting time entry

--- components/time-entry/TimeEntry.jsx
+++ components/time-entry/TimeEntry.jsx
@@ -3,32 +3,46 @@ import PropTypes from 'prop-types';
 
 import './time-entry.scss';
 
-const TimeEntry = ({
-  client, endTime, id, onDelete, startTime
-}) => (
-  <React.Fragment>
-    <ul className="entry-content">
-      <li className="entry-content__activity">
-        <div className="entry-content__client">{client}</div>
-        <button
-          onClick={() => onDelete(id)}
-          className="entry-content__button"
-          type="button"
-        >
-          <img
-            alt="Delete icon"
-            className="entry-content__button-icon"
-            src="/static/images/icon-delete.svg"
-          />
-          Delete
-        </button>
-        <div className="entry-content__time">
-          {`${startTime} - ${endTime}`}
-        </div>
-      </li>
-    </ul>
-  </React.Fragment>
-);
+class TimeEntry extends React.Component {
+  confirmDelete = (id) => {
+    const { onDelete } = this.props;
+    const askConfirm = window.confirm('Do you want to delete this entry?');
+    if (askConfirm) {
+      onDelete(id);
+    }
+  }
+
+  render() {
+    const {
+      client, endTime, id, startTime
+    } = this.props;
+
+    return (
+      <React.Fragment>
+        <ul className="entry-content">
+          <li className="entry-content__activity">
+            <div className="entry-content__client">{client}</div>
+            <button
+              onClick={() => this.confirmDelete(id)}
+              className="entry-content__button"
+              type="button"
+            >
+              <img
+                alt="Delete icon"
+                className="entry-content__button-icon"
+                src="/static/images/icon-delete.svg"
+              />
+              Delete
+            </button>
+            <div className="entry-content__time">
+              {`${startTime} - ${endTime}`}
+            </div>
+          </li>
+        </ul>
+      </React.Fragment>
+    );
+  }
+}
 
 TimeEntry.propTypes = {
   client: PropTypes.string.isRequired,
